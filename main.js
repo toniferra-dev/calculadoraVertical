25d7dcf92366ce822178f089cc34db14e42b77bb
Toni Dev <70471508+toniferra-dev@users.noreply.github.com>
2024-07-15 16:54:26 +0200

    inicio

---
+++ main.js
@@ -0,0 +1,15 @@
+// detectar la rotación de la pantalla del movil, para saltar a calculadora.html
+// si la pantalla está en vertical, se queda en index.html
+// si la pantalla está en horizontal, se va a calculadora.html
+
+// detectar la rotación de la pantalla del movil
+window.addEventListener("orientationchange", function () {
+    // si la pantalla está en vertical, se queda en index.html
+    if (screen.orientation.type === "portrait-primary") {
+        window.location.href = "index.html";
+    }
+    // si la pantalla está en horizontal, se va a calculadora.html
+    else if (screen.orientation.type === "landscape-primary") {
+        window.location.href = "calculadora.html";
+    }
+});
